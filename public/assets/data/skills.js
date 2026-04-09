ed9ea352bbeaa76f1ac58a29f3b35838dd6c9cfa
Manas <manaskalamkar910@gmail.com>
2026-04-09 16:30:54 +0530

    added projects and skills data

---
+++ public/assets/data/skills.js
@@ -0,0 +1,22 @@
+import { FaHtml5 } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { BiLogoTypescript } from "react-icons/bi";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaReact } from "react-icons/fa";
+import { FaJava } from "react-icons/fa";
+import { SiExpress } from "react-icons/si";
+import { FaDatabase } from "react-icons/fa";
+
+export const skills = [
+    {name:"HTML5", color:"#f16529" ,Icon:FaHtml5 },
+    {name:"tailwindcss", color:"#2596be" ,Icon:RiTailwindCssFill},
+    {name:"TypeScript", color:"#3178c6" ,Icon:BiLogoTypescript},
+    {name:"JavaScript", color:"#f7df1e" ,Icon:IoLogoJavascript},
+    {name:"React", color:"#58c4dc" ,Icon:FaReact},
+    {name:"Java", color:"#f29111" ,Icon:FaJava},
+]
+
+export const futureSkills = [
+    {name:"Express", color:"#6cb52d" ,Icon:SiExpress },
+    {name:"Databases", color:"#c74634" ,Icon:FaDatabase},
+]
