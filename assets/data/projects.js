15bdfa01f3d4e3e3fcb74528bdc1ac6588c324a0
Manas <manaskalamkar910@gmail.com>
2026-04-10 15:42:54 +0530

    Updates

---
+++ assets/data/projects.js
@@ -0,0 +1,69 @@
+
+export const projects = [
+  {
+    id: 1,
+    title: "Clicker Game",
+    description: "An interactive game using Event-Driven Programming, with a custom game loop to track scores in real-time. Optimized for mobile devices with Touch Events for a seamless experience.",
+    tech: ["JavaScript", "CSS", "HTML"],
+    live: "https://manas-kalamkar.github.io/Clicker/",
+    github: "https://github.com/Manas-Kalamkar/Clicker",
+    img: "./assets/data/images/projects/clicker.webp"
+  },
+  {
+    id: 2,
+    title: "Stopwatch",
+    description: "A high-precision timer built with Vanilla JavaScript, utilizing efficient DOM manipulation and setInterval for smooth Start, Stop, and Lap functions with zero latency.",
+    tech: ["CSS", "JavaScript", "HTML"],
+    live: " https://manas-kalamkar.github.io/project_3_stopwatch/",
+    github: "https://github.com/Manas-Kalamkar/project_3_stopwatch",
+    img: "./assets/data/images/projects/stopwatch.webp"
+  },
+  {
+    id: 3,
+    title: "Todo App",
+    description: "A dynamic task management app with full CRUD functionality, built with JavaScript and CSS3. Uses event delegation and local storage for persistent user data across sessions.",
+    tech: ["CSS", "JavaScript", "HTML"],
+    live: "https://manas-kalamkar.github.io/project_2_todo/",
+    github: "https://github.com/Manas-Kalamkar/project_2_todo",
+    img: "./assets/data/images/projects/todo.webp"
+  },
+  {
+    id: 4,
+    title: "Responsive Portfolio Website",
+    description: "A modern, responsive portfolio site developed with React and Tailwind CSS, featuring a flexible grid layout, smooth navigation, and semantic HTML5 for SEO and accessibility.",
+    tech: ["HTML", "CSS"],
+    live: "https://manas-kalamkar.github.io/project_1_responsive_design/",
+    github: "https://github.com/Manas-Kalamkar/project_1_responsive_design",
+    img: "./assets/data/images/projects/responsive.webp"
+  },
+  {
+    id: 5,
+    title: "Weather App",
+    description: "A responsive weather dashboard built with React and Tailwind CSS, integrating OpenWeatherMap API to fetch real-time climate and air quality data for a seamless user experience.",
+    tech: ["React", "Tailwindcss"],
+    live: "https://manas-kalamkar.github.io/project_5_weather_react_app/",
+    github: "https://github.com/Manas-Kalamkar/project_5_weather_react_app",
+    img: "./assets/data/images/projects/weather.webp"
+  },
+  {
+    id: 6,
+    title: "MK Blog Platform",
+    description: "This repository contains my solution for the CA Monk Frontend Internship Blog Application Website.",
+    tech: ["React", "TypeScript", "Supabase","TanStack Query(V5)"],
+    live: "https://manas-kalamkar.github.io/mkblog/",
+    github: "https://github.com/Manas-Kalamkar/mkblog",
+    img: "./assets/data/images/projects/mkblog.webp"
+  },
+  {
+    id: 7,
+    title: "E-commerce App",
+    description: "A modern, responsive E-commerce web application built using React with a strong focus on clean UI, reusable components, and scalable frontend architecture.",
+    tech: ["React", "TypeScript", "Style Components","React Router"],
+    live: "https://manas-kalamkar.github.io/ecommerce/",
+    github: "https://github.com/Manas-Kalamkar/ecommerce",
+    img: "./assets/data/images/projects/ecom.webp"
+  },
+
+
+];
+
---
+++ assets/data/workstation.js
@@ -0,0 +1,30 @@
+export const workStation = [
+    {   
+        id:1,
+        title:"VS Code (Code Editor)",
+        specs:"My primary environment, fine-tuned with ESLint and Prettier for clean, consistent React and TypeScript code",
+        position:"col-start-1 row-start-1",
+
+    },
+    {
+        id:2,
+        title:"Git & GitHub (Version Control)",
+        specs:"The foundation of all projects. Used daily for branching, merging, and collaborative code management.",
+        position:"col-start-3 row-start-1",
+
+    },
+    {
+        id:3,
+        title:"Mouse",
+        specs:"not a gaming mouse but looks like one",
+        position:"col-start-2 row-start-2",
+
+    },
+    {
+        id:4,
+        title:"water bottle",
+        specs:"to keep me hydrated",
+        position:"col-start-4 row-start-2",
+
+    },
+]
