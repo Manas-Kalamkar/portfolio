e9c6d4fbf3897181f11336ae449f875a513ca98b
Manas <manaskalamkar910@gmail.com>
2026-04-10 08:47:10 +0530

    feat: App.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React, { useEffect } from 'react'
 import Header from './components/Header';
 import Main from './components/Main';
-import useLenis from './components/lenis';
+import { BrowserRouter } from 'react-router-dom';
+import './App.css'
+import DarkLightThemeProvider from './components/DarkLightThemeProvider';
 
 
 const App = () => {
   return (
     <div className='w-full '>
-      <Header/>
-      <Main />
+      <BrowserRouter>
+        <DarkLightThemeProvider>
+          <Header />
+          <Main />
+        </DarkLightThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
