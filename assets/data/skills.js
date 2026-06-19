628b5302a1d87a50dbb6f39ec81af1933328e47e
Manas Kalamkar <itsmanas.codes@gmail.com>
2026-06-19 08:32:05 +0530

    Updates

--- assets/data/skills.js
+++ assets/data/skills.js
@@ -5,17 +5,17 @@ import { IoLogoJavascript } from "react-icons/io5";
 import { SiDocker, SiExpress } from "react-icons/si";
 
 export const skills = [
-    { name: "HTML5", color: "#f16529", Icon: FaHtml5 },
-    { name: "tailwindcss", color: "#2596be", Icon: RiTailwindCssFill },
-    { name: "TypeScript", color: "#3178c6", Icon: BiLogoTypescript },
-    { name: "JavaScript", color: "#f7df1e", Icon: IoLogoJavascript },
-    { name: "React", color: "#149eca", Icon: FaReact },
+    { name: "HTML5", color: "#E34F26", Icon: FaHtml5 },
+    { name: "Tailwind CSS", color: "#06B6D4", Icon: RiTailwindCssFill },
+    { name: "TypeScript", color: "#3178C6", Icon: BiLogoTypescript },
+    { name: "JavaScript", color: "#F7DF1E", Icon: IoLogoJavascript },
+    { name: "React", color: "#61DAFB", Icon: FaReact },
     { name: "Git", color: "#F05032", Icon: FaGitAlt },
-    { name: "GitHub", color: "#8b949e", Icon: FaGithub },
+    { name: "GitHub", color: "#c5c6c9", Icon: FaGithub },
 ];
 
 export const futureSkills = [
-    { name: "Docker", color: "#58c4dc", Icon: SiDocker },
-    { name: "Express", color: "#6cb52d", Icon: SiExpress },
-    { name: "Databases", color: "#c74634", Icon: FaDatabase },
+    { name: "Docker", color: "#2496ED", Icon: SiDocker },
+    { name: "Express", color: "#c5c6c9", Icon: SiExpress },
+    { name: "Databases", color: "#47A248", Icon: FaDatabase }, // MongoDB-inspired
 ];
